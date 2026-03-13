4808755decd72ac3598e54c59061147992f72c23
Vaishnav Manoj <vnav.m69@gmail.com>
2026-03-13 03:41:44 -0400

    Implemented Mongo API endpoints

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,26 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find({});
+        res.json(documents);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships' });
+      }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    const state = req.params.state;
+    const dealers = await Dealerships.find({ state: state });
+    res.json(dealers);
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    const dealerId = req.params.id;
+    const dealer = await Dealerships.find({ id: dealerId });
+    res.json(dealer);
 });
 
 //Express route to insert review
